0111331494394cfc0d8d808f2bdbb68df2882b19
Jonathan Keller <keller.jonathan@gmail.com>
2017-05-09 19:09:05 -0700

    Refactoring in preparation for implementation.

--- tt-edit.js
+++ tt-edit.js
@@ -29,45 +29,54 @@ function* performUpdate(entry) {
   //   }
 }
 
+function* getEntryToEdit() {
+  const r = yield* db.timeEntry.get(entryDate);
+
+  if (r && r.length) {
+    debug(JSON.stringify(r, null, 2));
+  } else {
+    console.log(chalk.yellow(`No Time Entries Defined for ${moment(entryDate).format('YYYY-MM-DD')}`));
+  }
+  if (r) {
+    debug(JSON.stringify(r, null, 2));
+    const answer = yield inquirer.prompt([
+      {
+        name: 'entry',
+        type: 'list',
+        message: 'Select the Time Entry to Edit',
+        choices: r.map(item => ({
+          value: item._id,
+          name: sprintf('%-8.8s : %-20.20s : %4i : %-15.15s : %-15.15s',
+            moment(item.insertTime).format('h:mm a'),
+            item.entryDescription,
+            item.minutes,
+            item.project,
+            item.timeType) })),
+      },
+    ]);
+    return r.find(e => (e._id === answer.entry));
+  }
+  console.log(chalk.yellow(`No Time Entries Entered for ${entryDate}`));
+  return null;
+}
+
+function* handleEntryChanges(entry) {
+
+}
 
 co(function* run() {
   let entry = null;
   if (!editLast) {
-    const r = yield* db.timeEntry.get(entryDate);
-
-    if (r && r.length) {
-      debug(JSON.stringify(r, null, 2));
-    } else {
-      console.log(chalk.yellow(`No Time Entries Defined for ${moment(entryDate).format('YYYY-MM-DD')}`));
-    }
-    if (r) {
-      debug(JSON.stringify(r, null, 2));
-      const answer = yield inquirer.prompt([
-        {
-          name: 'entry',
-          type: 'list',
-          message: 'Select the Time Entry to Edit',
-          choices: r.map(item => ({
-            value: item._id,
-            name: sprintf('%-8.8s : %-20.20s : %4i : %-15.15s : %-15.15s',
-              moment(item.insertTime).format('h:mm a'),
-              item.entryDescription,
-              item.minutes,
-              item.project,
-              item.timeType) })),
-        },
-      ]);
-      entry = r.find(e => (e._id === answer.entry));
-    } else {
-      console.log(chalk.yellow(`No Time Entries Entered for ${entryDate}`));
-    }
+    entry = yield* getEntryToEdit();
   } else {
     entry = yield* db.timeEntry.getMostRecentEntry();
   }
   debug(`Entry Selected: ${JSON.stringify(entry)}`);
 
   // TODO: implement the edit UI
+  entry = yield* handleEntryChanges(entry);
   // TODO: Update the record in the database - new db API method
+  yield* performUpdate(entry);
 });
 
 
