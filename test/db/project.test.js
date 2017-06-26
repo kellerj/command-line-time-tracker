31e78d90601171b7a6311c769c4df7c5e8ea5e92
Jonathan Keller <keller.jonathan@gmail.com>
2017-06-25 17:39:35 -0700

    Getting async tests with mocks working.

--- test/db/project.test.js
+++ test/db/project.test.js
@@ -1,9 +1,7 @@
 const expect = require('chai').expect;
 const assert = require('chai').assert;
-// const moment = require('moment');
-// const sinon = require('sinon');
 const spy = require('sinon').spy;
-const mock = require('sinon').mock;
+// const mock = require('sinon').mock;
 const stub = require('sinon').stub;
 const co = require('co');
 
@@ -11,26 +9,40 @@ const cursor = {
   // eslint-disable-next-line no-unused-vars
   sort: sortObj => cursor,
   // eslint-disable-next-line no-unused-vars
-  toArray: () => [],
+  toArray: () => [
+    { _id: '1', name: 'Project 1' },
+    { _id: '2', name: 'Project 2' },
+    { _id: '3', name: 'Project 3' },
+    { _id: '4', name: 'Project 4' },
+  ],
 };
 
 const collection = {
-  // eslint-disable-next-line no-unused-vars
-  find: queryObj => cursor,
+  // eslint-disable-next-line no-unused-vars,require-yield
+  find(queryObj) { return cursor; },
+  // eslint-disable-next-line no-unused-vars,require-yield
+  * findOne(queryObj) { return null; },
+  // eslint-disable-next-line no-unused-vars,require-yield
+  * insertOne(obj) { return { insertedCount: 1 }; },
 };
 
+spy(collection, 'insertOne');
+// spy(collection, 'findOne');
+spy(collection, 'find');
+
 const db = {
-  // eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line no-unused-vars,arrow-body-style
   collection: (collectionName) => {
-    console.log('db.collection');
+    // console.log('db.collection');
     return collection;
   },
   close: () => {
-    console.log('db.close');
+    // console.log('db.close');
   },
 };
 
-const closeSpy = spy(db, 'close');
+spy(db, 'collection');
+spy(db, 'close');
 
 // eslint-disable-next-line require-yield
 function* getConnection() {
@@ -41,14 +53,19 @@ const project = require('../../db/project')(getConnection);
 
 describe('db/project', () => {
   describe('getAll', () => {
-    // it('returns a list of all project names returned from MongoDB', () => {
-    //   co(project.getAll());
-    // });
+    it('returns a list of all project names returned from MongoDB', async () => {
+      const result = await co(project.getAll());
+      expect(collection.find.called).to.equal(true, 'did not call find on the collection');
+      expect(result.length).to.equal(cursor.toArray().length);
+      expect(JSON.stringify(result, null, 2)).to.equal(JSON.stringify(cursor.toArray(), null, 2));
+    });
     // ES7 Version
     it('opens and closes the database connection', async () => {
       await co(project.getAll());
-      console.log(`Called: ${db.close.called}`);
-      assert(closeSpy.called, 'db.close was not called');
+      // console.log(`Called: ${db.close.called}`);
+      // console.log(result);
+      expect(db.collection.called).to.equal(true, 'did not have db connection - did not obtain collection reference');
+      expect(db.close.called).to.equal(true, 'db.close was not called');
     });
     // ES6 version of above function
     // it('opens and closes the database connection', () => {
@@ -63,8 +80,33 @@ describe('db/project', () => {
     //   //   // assert.fail('', '', 'unimplemented test');
     //   // });
     // });
-    // it('sorts the projects by name', () => {
-    //   assert.fail('', '', 'unimplemented test');
-    // });
+  });
+  describe('getAll', () => {
+    it('calls MongoDB to insert the project', async () => {
+      const result = await co(project.insert('A New Project'));
+      expect(collection.insertOne.called).to.equal(true, 'did not call insertOne on the collection');
+
+      expect(result).to.equal(true, 'Should have returned true after inserting record');
+    });
+    it('returns false when the project already exists', async () => {
+      stub(collection, 'findOne').callsFake(() => ({ _id: '5', name: 'A New Project' }));
+      const result = await co(project.insert('A New Project'));
+      collection.findOne.restore();
+      expect(result).to.equal(false, 'Should have returned false since record exists.');
+    });
+
+    it('throws an AeertionError when mongo reports an insert failure', async () => {
+      collection.insertOne.restore();
+      stub(collection, 'insertOne').callsFake(() => ({ insertedCount: 0 }));
+      await co(project.insert('A New Project')).then(() => {
+        assert.fail('Should have thrown an exception');
+      }).catch((err) => {
+        // do nothing
+        expect(err.name).to.equal('AssertionError');
+      });
+      collection.insertOne.restore();
+      spy(collection, 'insertOne');
+      // expect(result).to.throw();
+    });
   });
 });
