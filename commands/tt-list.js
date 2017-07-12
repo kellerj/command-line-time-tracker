62b0b8391bbf1f77d1f30a4e39875d43c1038562
Jonathan Keller <keller.jonathan@gmail.com>
2017-07-12 07:27:08 -0700

    Updated to use exception handling instead of process.exit

--- commands/tt-list.js
+++ commands/tt-list.js
@@ -10,7 +10,8 @@ const validations = require('../utils/validations');
 const displayUtils = require('../utils/display-utils');
 const debug = require('debug')('tt:ls');
 
-commander
+try {
+  commander
     .version('1.0.0')
     .description('List time entries in a tabular format.')
     //.option('--csv', 'Output in a CSV format instead of ASCII table.')
@@ -21,39 +22,43 @@ commander
     .option('--last', 'When no date is specified, use yesterday\'s date')
     .parse(process.argv);
 
-const noDateOutput = commander.nodate;
+  const noDateOutput = commander.nodate;
 
-const { startDate, endDate, errorMessage } = validations.getStartAndEndDates(commander);
-if (errorMessage) {
-  console.log(chalk.red(errorMessage));
-  process.exit(-1);
-}
-
-co(function* run() {
-  const r = yield* db.timeEntry.get(startDate, endDate);
-
-  if (r && r.length) {
-    debug(JSON.stringify(r, null, 2));
-    const t = new Table();
-    r.forEach((item) => {
-      if (!noDateOutput) {
-        t.cell('Date', item.entryDate, displayUtils.entryDatePrinter);
-      }
-      t.cell('Logged', item.insertTime, displayUtils.insertTimePrinter);
-      t.cell('Project', item.project ? item.project : '');
-      t.cell('Type', item.timeType ? item.timeType : '');
-      t.cell('Time', item.minutes ? item.minutes : 0, displayUtils.timePrinter);
-      t.cell('Description', item.entryDescription);
-      t.cell(' ', item.wasteOfTime ? '💩' : '');
-      t.newRow();
-    });
-    t.total('Time', {
-      printer: displayUtils.timePrinter,
-    });
-    console.log(t.toString());
-  } else {
-    console.log(chalk.yellow(`No Time Entries Defined for ${moment(startDate).format('YYYY-MM-DD')}`));
+  const { startDate, endDate, errorMessage } = validations.getStartAndEndDates(commander);
+  if (errorMessage) {
+    throw new Error(errorMessage);
   }
-}).catch((err) => {
-  console.log(chalk.bgRed(err.stack));
-});
+
+  co(function* run() {
+    const r = yield* db.timeEntry.get(startDate, endDate);
+
+    if (r && r.length) {
+      debug(JSON.stringify(r, null, 2));
+      const t = new Table();
+      r.forEach((item) => {
+        if (!noDateOutput) {
+          t.cell('Date', item.entryDate, displayUtils.entryDatePrinter);
+        }
+        t.cell('Logged', item.insertTime, displayUtils.insertTimePrinter);
+        t.cell('Project', item.project ? item.project : '');
+        t.cell('Type', item.timeType ? item.timeType : '');
+        t.cell('Time', item.minutes ? item.minutes : 0, displayUtils.timePrinter);
+        t.cell('Description', item.entryDescription);
+        t.cell(' ', item.wasteOfTime ? '💩' : '');
+        t.newRow();
+      });
+      t.total('Time', {
+        printer: displayUtils.timePrinter,
+      });
+      console.log(t.toString());
+    } else {
+      console.log(chalk.yellow(`No Time Entries Defined for ${moment(startDate).format('YYYY-MM-DD')}`));
+    }
+  }).catch((err) => {
+    console.log(chalk.bgRed(err.stack));
+  });
+} catch (err) {
+  // do nothing - just suppress the error trace
+  console.log(chalk.red(err.message));
+  debug(err);
+}
