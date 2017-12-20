11b3015c9f99f6020c100e94a4b57c5dceb1381c
Jonathan Keller <keller.jonathan@gmail.com>
2017-12-20 08:01:34 -0800

    In progress work on adding markdown format to the summary output.

--- src/commands/tt-summary.js
+++ src/commands/tt-summary.js
@@ -22,7 +22,10 @@ commander
   .option('--last', 'Change the day, week, or month criteria to the prior week or month.')
   .option('-y, --yesterday', 'When no date is specified, use yesterday\'s date')
   .option('--noHeader', 'Don\'t print the header with the dates above the summary')
+  .option('-m, --markdown', 'Output the table using markdown formatting')
   .parse(process.argv);
+
+// LOG(commander);
 
 const { startDate, endDate, errorMessage } = validations.getStartAndEndDates(commander);
 if (errorMessage) {
@@ -37,18 +40,25 @@ async function run() {
     } else {
       reportHeader = `Time Summary Report for ${displayUtils.entryDatePrinter(startDate)} through ${displayUtils.entryDatePrinter(endDate)}`;
     }
-    console.log(chalk.yellow('-'.repeat(reportHeader.length)));
-    console.log(chalk.yellow(reportHeader));
-    console.log(chalk.yellow('-'.repeat(reportHeader.length)));
+    if (commander.markdown) {
+      process.stdout.write(`## ${reportHeader}\n\n`);
+    } else {
+      process.stdout.write(chalk.yellow('-'.repeat(reportHeader.length)));
+      process.stdout.write('\n');
+      process.stdout.write(chalk.yellow(reportHeader));
+      process.stdout.write('\n');
+      process.stdout.write(chalk.yellow('-'.repeat(reportHeader.length)));
+      process.stdout.write('\n');
+    }
   }
 
   const r = await db.timeEntry.summarizeByProjectAndTimeType(startDate, endDate);
   if (!r.length) {
-    console.log(chalk.yellow('There are no time entries to summarize for the given period.'));
+    process.stdout.write(chalk.yellow('There are no time entries to summarize for the given period.'));
+    process.stdout.write('\n');
     return;
   }
   LOG(JSON.stringify(r, null, 2));
-  // eslint-disable-next-line no-param-reassign
   const totalTime = r.reduce((acc, item) => (acc + item.minutes), 0);
   // need to transform the structure into a new grid format - group by project
   // and build a record with keys for each time type
@@ -71,38 +81,89 @@ async function run() {
     return acc;
   }, []);
   grid = grid.sort(displayUtils.sortOtherLast);
-  const t = new Table();
-  grid.forEach((item) => {
-    t.cell('Project', item.project ? item.project : '');
-    let projectTotal = 0;
+  if (!commander.markdown) {
+    const t = new Table();
+    grid.forEach((item) => {
+      t.cell('Project', item.project ? item.project : '');
+      let projectTotal = 0;
+      headings.forEach((heading) => {
+        t.cell(heading, item[heading] ? item[heading] : 0, displayUtils.timePrinter);
+        projectTotal += item[heading] ? item[heading] : 0;
+      });
+      t.cell('Totals', projectTotal, displayUtils.timeAndPercentPrinter(totalTime));
+      // t.cell('   %', Table.padLeft(`${Math.round(100 * (projectTotal / totalTime), 0)}%`, 4));
+      t.newRow();
+    });
     headings.forEach((heading) => {
-      t.cell(heading, item[heading] ? item[heading] : 0, displayUtils.timePrinter);
-      projectTotal += item[heading] ? item[heading] : 0;
+      t.total(heading, {
+        printer: displayUtils.timeAndPercentPrinter(totalTime),
+      });
     });
-    t.cell('Totals', projectTotal, displayUtils.timeAndPercentPrinter(totalTime));
-    // t.cell('   %', Table.padLeft(`${Math.round(100 * (projectTotal / totalTime), 0)}%`, 4));
-    t.newRow();
-  });
-  headings.forEach((heading) => {
-    t.total(heading, {
-      printer: displayUtils.timeAndPercentPrinter(totalTime),
+    t.total('Totals', {
+      printer: displayUtils.timePrinter,
     });
-  });
-  t.total('Totals', {
-    printer: displayUtils.timePrinter,
-  });
 
-  console.log(t.toString());
+    process.stdout.write(t.toString());
+    process.stdout.write('\n');
+  } else {
+    LOG(`Data Grid: ${JSON.stringify(grid, null, 2)}`);
+    const colInfo = [];
+    const projectColumnWidth = grid.reduce((maxWidth, gridRow) =>
+      (gridRow.project.length > maxWidth ? gridRow.project.length : maxWidth), 0);
+    colInfo.push({ align: 'left', width: projectColumnWidth, heading: 'project' });
+    // headers
+    process.stdout.write('| ');
+    process.stdout.write('Project'.padEnd(projectColumnWidth));
+    process.stdout.write(' | ');
+    headings.forEach((heading) => {
+      process.stdout.write(heading);
+      process.stdout.write(' | ');
+      colInfo.push({ align: 'right', width: heading.length, heading });
+    });
+    process.stdout.write('\n');
+    // dividing line
+    process.stdout.write('| ');
+    colInfo.forEach((info) => {
+      if (info.align === 'left') {
+        process.stdout.write(':');
+      }
+      process.stdout.write('-'.repeat(info.width - 1));
+      if (info.align === 'right') {
+        process.stdout.write(':');
+      }
+      process.stdout.write(' | ');
+    });
+    process.stdout.write('\n');
+    // summary data
+    grid.forEach((row) => {
+      // LOG(row);
+      process.stdout.write('| ');
+      colInfo.forEach((col) => {
+        if (!row[col.heading]) {
+          process.stdout.write(' '.repeat(col.width));
+        } else if (col.align === 'left') {
+          process.stdout.write(row[col.heading].toString().padEnd(col.width));
+        } else {
+          process.stdout.write(row[col.heading].toString().padStart(col.width));
+        }
+        process.stdout.write(' | ');
+      });
+      process.stdout.write('\n');
+    });
+    process.stdout.write('\n');
+  }
 }
 
 try {
   run().catch((err) => {
-    console.log(chalk.red(err.message));
-    LOG(err);
+    process.stderr.write(chalk.red(err.message));
+    process.stderr.write('\n');
     process.exitCode = 1;
+    LOG(err);
   });
 } catch (err) {
-  console.log(chalk.red(err.message));
-  LOG(err);
+  process.stderr.write(chalk.red(err.message));
+  process.stderr.write('\n');
   process.exitCode = 1;
+  LOG(err);
 }
