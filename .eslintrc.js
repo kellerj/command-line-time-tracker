dff1ff869804b4a06096357f071d644436eb745a
Jonathan Keller <keller.jonathan@gmail.com>
2018-02-11 12:32:54 -0800

    Updating max len eslint setting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
 module.exports = {
   extends: "airbnb-base",
   rules: {
-    "no-console": "off",
     "spaced-comment": "off",
     "no-underscore-dangle": "off",
     "no-param-reassign": ["error", { "props": false }],
     "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+    "max-len": ["warn", 100, 2, {
+      ignoreUrls: true,
+      ignoreComments: true,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }]
   },
   env: {
     node: true,
